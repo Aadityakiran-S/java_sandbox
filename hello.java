a29387d9c7cce39bb7a888b47e49551d963b6420
Aadityakiran S <aadityakirans@gmail.com>
2023-12-05 10:53:02 +0530

    random

--- hello.java
+++ hello.java
@@ -1,13 +1,7 @@
 class Hello{
-<<<<<<< HEAD
-    public static void main(String a[]){
-        int num = 3;
-        System.out.println(num);
-=======
     public static void main(String mainInput[]){
         int a = 10; int b = 20;
         System.out.println(a + b);
         System.out.println("Hello World!");
->>>>>>> d4f91a450d60220dca5cbeab3e79e63fe654161f
     }    
 }
